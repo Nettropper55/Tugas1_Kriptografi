bbfb0374cf90ff9b735752240451db26f965cc9e
Albertus Stefan Hindrawan <93307801+Nettropper55@users.noreply.github.com>
2023-09-06 10:28:45 +0700

    Update Simple Encrypt.py

--- Simple Encrypt.py
+++ Simple Encrypt.py
@@ -1,8 +1,9 @@
 def caesar_cipher_encrypt(string, key):
     encrypted_string = ""
     proses = string
+    kunci_steps = []
 
-    for kunci in key:
+    for kunci in key.lower():  # Konversi kunci menjadi huruf kecil
         encrypted_string = ""
         for char in proses:
             if char.isalpha():  # Periksa apakah karakter adalah huruf
@@ -11,19 +12,26 @@ def caesar_cipher_encrypt(string, key):
                 else:
                     shift = ord(kunci) - ord('a')  # Menghitung pergeseran berdasarkan urutan karakter kunci
                 if char.isupper():
-                    encrypted_char = chr(((ord(char) - ord('A') + shift)%26 ) + ord('A'))
+                    encrypted_char = chr(((ord(char) - ord('A') + shift) % 26) + ord('a'))  # Konversi menjadi huruf kecil
                 else:
-                    encrypted_char = chr(((ord(char) - ord('a') + shift)%26 ) + ord('a'))
+                    encrypted_char = chr(((ord(char) - ord('a') + shift) % 26) + ord('a'))
             else:
-                encrypted_char = char
+                print("Error: Input mengandung spasi atau angka.")
+                return None
             encrypted_string += encrypted_char
+        kunci_steps.append(encrypted_string)  # Tambahkan langkah perubahan kunci
         proses = encrypted_string
-    return encrypted_string
 
+    # Menampilkan langkah perubahan kunci
+    for i, step in enumerate(kunci_steps):
+        print(f"Karakter ke-{i+1} kunci: {step} ({key[i]})")
+
+    return encrypted_string
 
 # Contoh penggunaan
 teks = input("Masukkan teks yang ingin dienkripsi: ")
 kunci = input("Masukkan kunci: ")
 
 hasil_enkripsi = caesar_cipher_encrypt(teks, kunci)
-print("Teks terenkripsi:", hasil_enkripsi)
+if hasil_enkripsi is not None:
+    print("Teks terenkripsi:", hasil_enkripsi)
